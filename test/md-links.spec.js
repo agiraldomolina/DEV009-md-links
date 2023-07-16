28ebd7c98d8867326e478e82b3ab7bba221742f9
Alba Giraldo <agiraldomolina@gmail.com>
2023-07-16 14:55:59 -0400

    69% lines tested

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -14,6 +14,62 @@ const pathWithLinks='mdFiles\\test3.md' // valid path with links
 const pathWithoutLinks='mdFiles\\test4.md' // valid path but empty file
 const pathNotMDFile='mdFiles\\myText2.txt'
 
+const objectLinksExample=[
+  {
+    text: 'Generalidades del protocolo HTTP - MDN',
+    url: 'https://developer.mozilla.org/es/docs/Web/HTTP/Overview',
+    file: 'C:\\Users\\albag\\OneDrive\\Escritorio\\mdLinks\\DEV009-md-links\\test1.md'
+  },
+  {
+    text: 'Mensajes HTTP - MDN',
+    url: 'https://developer.mozilla.org/es/docs/Web/HTTP/Messages',
+    file: 'C:\\Users\\albag\\OneDrive\\Escritorio\\mdLinks\\DEV009-md-links\\test1.md'
+  },
+  {
+    text: 'Link roto',
+    url: 'https://www.youtube.com/01RHn23Bn_0',
+    file: 'C:\\Users\\albag\\OneDrive\\Escritorio\\mdLinks\\DEV009-md-links\\test1.md'
+  },
+  {
+    text: 'Módulos, librerías, paquetes, frameworks... ¿cuál es la diferencia?',
+    url: 'http://community.laboratoria.la/t/modulos-librerias-paquetes-frameworks-cual-es-la-diferencia/175',
+    file: 'C:\\Users\\albag\\OneDrive\\Escritorio\\mdLinks\\DEV009-md-links\\test1.md'
+  }
+];
+
+const ObjectLinksWithStatus=[
+  {
+    text: 'Generalidades del protocolo HTTP - MDN',
+    url: 'https://developer.mozilla.org/es/docs/Web/HTTP/Overview',
+    file: 'C:\\Users\\albag\\OneDrive\\Escritorio\\mdLinks\\DEV009-md-links\\test1.md',
+    status: 200,
+    info: 'Valid'
+  },
+  {
+    text: 'Mensajes HTTP - MDN',
+    url: 'https://developer.mozilla.org/es/docs/Web/HTTP/Messages',
+    file: 'C:\\Users\\albag\\OneDrive\\Escritorio\\mdLinks\\DEV009-md-links\\test1.md',
+    status: 200,
+    info: 'Valid'
+  },
+  {
+    text: 'Link roto',
+    url: 'https://www.youtube.com/01RHn23Bn_0',
+    file: 'C:\\Users\\albag\\OneDrive\\Escritorio\\mdLinks\\DEV009-md-links\\test1.md',
+    status: 'Request failed with status code 404',
+    info: 'Broken'
+  },
+  {
+    text: 'Módulos, librerías, paquetes, frameworks... ¿cuál es la diferencia?',
+    url: 'http://community.laboratoria.la/t/modulos-librerias-paquetes-frameworks-cual-es-la-diferencia/175',
+    file: 'C:\\Users\\albag\\OneDrive\\Escritorio\\mdLinks\\DEV009-md-links\\test1.md',
+    status: 'getaddrinfo ENOTFOUND community.laboratoria.la',
+    info: 'Broken'
+  }
+];
+
+/////////////////////////////////////////////
+
 describe('readingFile',()=>{
   it("Should return an array with an object with one element",()=>{
 
@@ -32,7 +88,15 @@ describe('readingFile',()=>{
   })
 });
 
-describe('validateFoundedLinks')
+describe('validateFoundedLinks',()=>{
+  it('Should return an array with with four objects with information about their status link',()=>{
+
+    return validateFoundedLinks(objectLinksExample).then(result=>{
+      expect(result).toStrictEqual(ObjectLinksWithStatus)
+    })
+  })
+})
+
 
 
 it ("Should return true for path: './mdFiles/test3.md'", ()=>{
