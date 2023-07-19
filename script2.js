6252759b69fcbf989e3639eb8f1cd2448f850bea
Alba Giraldo <agiraldomolina@gmail.com>
2023-07-19 14:32:27 -0400

    Searching link in all files contained in a directory

--- script2.js
+++ script2.js
@@ -6,25 +6,54 @@ const {
     isMDFile,
     readingFile
 } =require ('./script1');
+const { list } = require('cowsay');
 
 const findMDFilesFromDir=(myPath)=>{
     let listFiles=[];
     myPath=path.resolve(myPath);
     if(fs.statSync(myPath).isDirectory()){
         const findedFiles=fs.readdirSync(myPath);
-
         findedFiles.forEach(file=>{
             listFiles=listFiles.concat(findMDFilesFromDir(`${myPath}\\${file}`))
         })
     }else{
         listFiles.push(myPath)
     }
-    return listFiles.filter(file=>isMDFile(file))
+    return listFiles.filter(file=>isMDFile(file));
 };
 
 console.log(typeof(findMDFilesFromDir));
 const path6='./mdFiles';
 console.log(findMDFilesFromDir(path6));
+
+
+const findLinksInMarkdownFiles = (myPath) => {
+    const mdFiles = findMDFilesFromDir(myPath);
+    const promises = mdFiles.map((file) => readingFile(file));
+  
+    return Promise.all(promises)
+      .then((results) => {
+        // Concatenamos todos los enlaces encontrados en un único array.
+        const links = results.reduce((acc, curr) => acc.concat(curr), []);
+        return links;
+      })
+      .catch((err) => {
+        console.error('Error:', err);
+        return [];
+      });
+  };
+  
+
+const myFolderPath = './mdFiles';
+findLinksInMarkdownFiles(myFolderPath)
+  .then((links) => {
+    console.log('Enlaces encontrados:');
+    console.log(links);
+    console.log(`${links.length} finded`);
+  })
+  .catch((err) => {
+    console.error('Error al buscar enlaces:', err);
+  });
 
 // const getContent=(myPath)=>{
 //     const isDir=fs.statSync(myPath).isDirectory();
@@ -49,28 +78,28 @@ console.log(findMDFilesFromDir(path6));
 
 
 
-const fecthLinksFromDir=(myPath)=>{
-    return new Promise((resolve,reject)=>{
-        const mdFilesFounded=findMDFilesFromDir(myPath);
-        console.log(mdFilesFounded);
-        if (mdFilesFounded.length>0){
-            const arrayWithAllLinks=mdFilesFounded.map(eachMDFile=>{
-                readingFile(eachMDFile).then(arrayLinks=>{arrayLinks
-                })
-            })
-            let singleArrayLinks=[];
-            Promise.all(arrayWithAllLinks).then(arrayLinks=>{
-                arrayLinks.forEach(array=>{
-                    singleArrayLinks=singleArrayLinks.concat(array);
-                })
-                resolve(Promise.all(singleArrayLinks))
-            })
+// const fecthLinksFromDir=(myPath)=>{
+//     return new Promise((resolve,reject)=>{
+//         const mdFilesFounded=findMDFilesFromDir(myPath);
+//         console.log(mdFilesFounded);
+//         if (mdFilesFounded.length>0){
+//             const arrayWithAllLinks=mdFilesFounded.map(eachMDFile=>{
+//                 readingFile(eachMDFile).then(arrayLinks=>{arrayLinks
+//                 })
+//             })
+//             let singleArrayLinks=[];
+//             Promise.all(arrayWithAllLinks).then(arrayLinks=>{
+//                 arrayLinks.forEach(array=>{
+//                     singleArrayLinks=singleArrayLinks.concat(array);
+//                 })
+//                 resolve(Promise.all(singleArrayLinks))
+//             })
             
-        }else{
-            reject(new Error ('MD files do not finded'))
-        }
-    })
-}
+//         }else{
+//             reject(new Error ('MD files do not finded'))
+//         }
+//     })
+// }
 
 
 
